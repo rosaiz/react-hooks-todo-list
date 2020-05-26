52506f828376ba6b1f66f7f4225024efe2e1d0c5
Ro <garciasaizrodrigo@gmail.com>
2020-05-26 12:21:36 -0300

    Initialize project using Create React App

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,89 @@
+//Import dependencies
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom'
+//Import components
+import TodoForm from './components/todo-form'
+import TodoList from './components/todo-list'
+//Import interfaces
+import { TodoInterface } from './interfaces'
+// Import styles
+import './styles/styles.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//TodoListApp component
+const TodoListApp = () => {
+    const [todos, setTodos] = React.useState<TodoInterface[]>([])
+
+    //Creating new todo item
+    function handleTodoCreate(todo: TodoInterface) {
+        //Prepare new todos state
+        const newTodosState: TodoInterface[] = [...todos]
+
+        //Update new todos state
+        newTodosState.push(todo)
+
+        //Update todos state
+        setTodos(newTodosState)
+
+    }
+
+    //Update existing todo item
+    function handleTodoUpdate(event: React.ChangeEvent<HTMLInputElement>, id: string) {
+        //Prepare new todos state
+        const newTodosState: TodoInterface[] = [...todos]
+
+        //Find correct todo item to update
+        newTodosState.find((todo: TodoInterface) => todo.id === id)!.text = event.target.value
+
+        //Update todos state
+        setTodos(newTodosState)
+    }
+
+    // Remove existing todo item
+    function handleTodoRemove(id: string) {
+        // Prepare new todos state
+        const newTodosState: TodoInterface[] = todos.filter((todo: TodoInterface) => todo.id !== id)
+        // Update todos state
+        setTodos(newTodosState)
+    }
+
+    // Check existing todo item as completed
+    function handleTodoComplete(id: string) {
+        // Copy current todos state
+        const newTodosState: TodoInterface[] = [...todos]
+        // Find the correct todo item and update its 'isCompleted' key
+        newTodosState.find((todo: TodoInterface) => todo.id === id)!.isCompleted = !newTodosState.find((todo: TodoInterface) => todo.id === id)!.isCompleted
+        // Update todos state
+        setTodos(newTodosState)
+    }
+    // Check if todo item has title
+    function handleTodoBlur(event: React.ChangeEvent<HTMLInputElement>) {
+        if (event.target.value.length === 0) {
+            event.target.classList.add('todo-input-error')
+        } else {
+            event.target.classList.remove('todo-input-error')
+        }
+    }
+
+    return (
+        <div className="todo-list-app">
+            {/* Todo form Component */}
+            <TodoForm
+                todos={todos}
+                handleTodoCreate={handleTodoCreate}
+            />
+
+            {/* Todo list component */}
+            <TodoList
+                todos={todos}
+                handleTodoUpdate={handleTodoUpdate}
+                handleTodoRemove={handleTodoRemove}
+                handleTodoComplete={handleTodoComplete}
+                handleTodoBlur={handleTodoBlur}
+            />
+        </div>
+    )
+}
+//Render the App in the DOM
+const rootElement = document.getElementById('root')
+render(<TodoListApp />, rootElement)
